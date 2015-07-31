51027ddc9dfe5b0b67f8f9ffc99ffe58dbfc453c
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-07-31 13:59:43 -0700

    Now exports QuizValidator & QuestionValidator.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 module.exports.isValidQuizDescriptor = require('./quizDescriptor').isValidQuizDescriptor;
+
+module.exports.QuizValidator = require('./validators/QuizValidator.js');
+module.exports.QuestionValidator = require('./validators/QuestionValidator.js');
