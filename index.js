1b456ea72919a4bf9c31ee410dae22c5b7b871bf
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-07-31 17:39:31 -0700

    No longer exports question validator.

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports.isValidQuizDescriptor = require('./quizDescriptor').isValidQuizDescriptor;
 
 module.exports.QuizValidator = require('./validators/QuizValidator.js');
-module.exports.QuestionValidator = require('./validators/QuestionValidator.js');
 
 module.exports.QuizBuilder = require('./QuizBuilder');
