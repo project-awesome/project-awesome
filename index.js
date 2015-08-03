890eccc03dbd777d57ab9ecdf6719dfefee4ebea
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-08-03 14:35:37 -0700

    Added isStringValidSeed.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports.isValidQuizDescriptor = require('./quizDescriptor').isValidQuizDescriptor;
 
+module.exports.isStringValidSeed = function(seed) {
+	return (seed.matches("[0-9A-F]+") && seed.length == 8);
+}
+
 module.exports.QuizValidator = require('./validators/QuizValidator.js');
 
 module.exports.QuizBuilder = require('./QuizBuilder');
