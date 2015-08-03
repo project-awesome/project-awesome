f95f7826f63609b30c588698fb6828c085605498
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-08-03 14:52:33 -0700

    Fixed isStringValidSeed regex.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports.isValidQuizDescriptor = require('./quizDescriptor').isValidQuizDescriptor;
 
-module.exports.isStringValidSeed = function(seed) {
-	return (typeof seed === 'string' && seed.matches("[0-9A-F]+") && seed.length == 8);
+module.exports.isStringValidSeed = function (seed) {
+	return (typeof seed === 'string' && seed.match(/^[a-fA-F0-9]{8}$/) !== null && seed.length == 8);
 }
 
 module.exports.QuizValidator = require('./validators/QuizValidator.js');
