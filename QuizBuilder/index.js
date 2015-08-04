796ff45a6448627364f61eb48042daa36bb5b959
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-08-04 13:13:29 -0700

    Refactored a bit. Added tests for QuizBuilder.

--- QuizBuilder/index.js
+++ QuizBuilder/index.js
@@ -1,36 +1,93 @@
 var tempQuizBuilder = require('../quiz');
+var randomModule = require("../random");
+var questionsModule =  require("../questions");
 
-function buildQuizFromQuizDescriptor(descriptor, id, hexStringSeed) {
+function getQuestions(descriptor, randomStream) {
+    var questions = [];
+    var n = descriptor["quiz"].length;
+
+    for(var i = 0; i < n; ++i) {
+
+        var item = descriptor.quiz[i];
+
+        if("question" in item) { //If this object is a question generator
+
+            var questionType = item.question;
+            var params = (("params" in item) ? item.params : {});
+            var repeat = (("repeat" in item) ? item.repeat : 1);
+            
+            var question = ((questionType in questionsModule.questionTypes) ? questionsModule.questionTypes[questionType] : null);
+
+            //Generate the specified number of the specified type of question, add them to the array
+            if(question != null) {
+                for(var j=0; j<repeat; j++) {
+                    var newQuestion = new question.f(randomStream,params);
+                    questions.push(newQuestion); 
+                }
+            }
+        }
+
+        /*  
+        //Allows you to provide an array of question-generating objects (just like a quiz) to generate n questions
+        //and choose k of those n at random.
+
+        if("chooseK" in item) {
+            var k = item.chooseK;
+            var itemsArray = (("items" in item) ? item.items : []);
+            var newArray = produceArrayOfQuestions(itemsArray, randomStream); //Recursively parse the array passed to chooseK
+            //After this finishes newArray should consist solely of question objects
+            
+            randomStream.shuffle(newArray);
+            questions = questions.concat(newArray.slice(0,k)); //Grab the first k elements and add them to the questions array
+        } // chooseK
+        
+        //Allows you to shuffle a list of questions.
+        //Essentially equivalent to a chooseK where K is the total number of questions generated,
+        //but this is a little clearer and nicer than having to use that trick.
+
+        if("shuffle" in item) {
+            var itemsArray = (("items" in item) ? item.items : []);
+            var newArray = produceArrayOfQuestions(itemsArray, randomStream);
+            
+            randomStream.shuffle(newArray);
+            questions = questions.concat(newArray);     
+        } // shuffle
+        
+        */
+
+    }
+    
+    return questions; 
+
+};
+
+function build(descriptor, id, hexStringSeed) {
     var quiz = {};
-    console.log(hexStringSeed);
     quiz.seed = hexStringSeed;
     var s = parseInt(hexStringSeed, 16);
-	var tmpQuiz = new tempQuizBuilder.Quiz(descriptor, { seed : s });
-
-    var showQuestions = 1;
-    var showKey = 1;
-    var showJson = 1;
-
+    var randomStream = new randomModule.random(s);
+	//var tmpQuiz = new tempQuizBuilder.Quiz(descriptor, { seed : s });
 
     quiz.title = descriptor.title;
     quiz.qd = id;
-    quiz.questions = [];
+    quiz.questions = getQuestions(descriptor, randomStream);
 
-
-	for (var i = 0; i < tmpQuiz.questions.length; i++) {
+    /*
+	for (var i = 0; i < questions.length; i++) {
     	var q = { 
-    		question : tmpQuiz.questions[i].question,
-    		answer : tmpQuiz.questions[i].answer,
-            format : tmpQuiz.questions[i].format
+    		question : questions[i].question,
+    		answer : questions[i].answer,
+            format : questions[i].format
     	};
     	quiz.questions.push(q);
 
 	}
+    */
 
 	return quiz;
 
 }
 
-
-module.exports.build = buildQuizFromQuizDescriptor;
+module.exports.getQuestions = getQuestions;
+module.exports.build = build;
 
--- test/QuizBuilder/index.js
+++ test/QuizBuilder/index.js
@@ -1,24 +1,46 @@
 var expect = require("chai").expect;
 var QuizValidator = require('../../validators/QuizValidator');
 var QuizBuilder = require('../../QuizBuilder');
+var randomModule = require("../../random");
 
 describe('QuizBuilder', function() {
+	var qd = {
+	    "version" : "0.1",
+	    "title" : "Example QuizJSON 1",
+	    "quiz": [{
+		    "question": "binHexOctDec",
+		    "repeat": 5,
+		}]
+	};
 
-	it('should produce a valid quiz', function() {
-		var qd = {
-		    "version" : "0.1",
-		    "title" : "Example QuizJSON 1",
-		    "quiz": [{
-			    "question": "binHexOctDec",
-			    "repeat": 5,
-			}]
-		}; 
+	describe('build(descriptor, id, hexStringSeed)', function() {
 
-		var quiz = QuizBuilder.build(qd, 1, '1234abef');
-		expect(QuizValidator.isValid(quiz)).to.be.true;
+		it('should produce a valid quiz', function() {
+			var quiz = QuizBuilder.build(qd, 1, '1234abef');
+			expect(QuizValidator.isValid(quiz)).to.be.true;
+		});
+
 	});
 
 
+	describe('getQuestions(descriptor, randomStream)', function() {
+		var retVals = [];
+		var mockRS = {
+			nextIntRange: function(n) { 
+				return retVals.shift();
+			}
+		};
+
+		it('should produce known set of questions when given randomStream', function() {
+			retVals = [0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+			var questions = QuizBuilder.getQuestions(qd, mockRS);
+			expect(questions).to.be.an('array');
+			expect(questions.length).to.equal(qd.quiz[0].repeat);
+			expect(questions[0].question).to.equal('Convert 5 from base 10 to base 2.');
+			expect(questions[0].answer).to.equal('101');
+		});
+
+	});
 
 
 });
