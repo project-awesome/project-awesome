005d81c5670caf17beb2a0d7131718230d2b1f02
Martin Wolfenbarger <wolfenbarger@umail.ucsb.edu>
2015-11-12 12:17:58 -0800

    new keyword no longer needed when calling generate

--- QuizBuilder/index.js
+++ QuizBuilder/index.js
@@ -21,7 +21,7 @@ function getQuestions(descriptor, randomStream) {
             //Generate the specified number of the specified type of question, add them to the array
             
             for(var j=0; j<repeat; j++) {
-                var newQuestion = new question.generate(randomStream, params);
+                var newQuestion = question.generate(randomStream, params);
                 questions.push(newQuestion); 
             }
         }
