e73a5d7974046c22c3c8593b33bc9146de1dd5bc
Phill Conrad <pconrad@cs.ucsb.edu>
2015-11-12 17:15:06 -0800

    additional tests for list

--- test/bin/pa/list.js
+++ test/bin/pa/list.js
@@ -34,4 +34,44 @@ describe('list', function() {
 			});
 		});
 
+		describe('action', function() {
+			describe('when projectAwesome.list is successful', function() {
+				var listStub, stdoutStub;
+				beforeEach(function() {
+					listStub = sinon.stub(projectAwesome, 'list', function() { return "liststuboutput"; });
+					stdoutStub = sinon.stub(process.stdout, 'write');
+				});
+				afterEach(function() {
+					listStub.restore();
+					stdoutStub.restore();
+				});
+				it('should call projectAwesome.list(type) and should output result + \n', function() {
+						pa_test_helper.run(pa,'list', ['a']);
+	                expect(listStub.calledOnce).to.be.true;
+	                expect(listStub.calledWith('a')).to.be.true;
+	                expect(stdoutStub.calledOnce).to.be.true;
+	                expect(stdoutStub.calledWith("liststuboutput\n")).to.be.true;
+				});
+			});
+			describe('when projectAwesome.list throws an error', function() {
+				var listStub, stdoutStub;
+				beforeEach(function() {
+					listStub = sinon.stub(projectAwesome, 'list', function() { throw "liststuberror"; });
+					stderrStub = sinon.stub(process.stderr, 'write');
+				});
+				afterEach(function() {
+					listStub.restore();
+					stderrStub.restore();
+				});
+				it('write error message + \n to stderr', function() {
+						pa_test_helper.run(pa,'list', ['a']);
+	                expect(listStub.calledOnce).to.be.true;
+	                expect(listStub.calledWith('a')).to.be.true;
+	                expect(stderrStub.calledOnce).to.be.true;
+	                expect(stderrStub.calledWith("liststuberror\n")).to.be.true;
+				});
+			});
+		});
+
+
 	});
