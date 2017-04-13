6cd89cbf4046f2a948b9c6c75a42e24414509959
Michelle Craig <mcraig@cs.toronto.edu>
2017-04-13 10:23:47 -0700

    works now with lousy presentation of pointer question and no answers -- time to refactor schema to allow nested lists but pushing a version before I continue

--- problemTypes/index.js
+++ problemTypes/index.js
@@ -2,7 +2,8 @@
 var problemTypes= {
     "fr-change-of-base": require("paq-fr-change-of-base"),
     "mc-change-of-base": require("paq-mc-change-of-base"),
-    "change-of-base": require("paq-change-of-base")
+    "change-of-base": require("paq-change-of-base"),
+    "C-pointer-types": require("paq-C-pointer-types")
 };
 
 module.exports.problemTypes = problemTypes;
