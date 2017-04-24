1516fbb72de964c4a85a37ca5cb9bbb764e9ad73
Phill Conrad <pconrad@cs.ucsb.edu>
2017-04-24 14:39:33 -0700

    PC/MC WIP continue adding tests in test/MoodleExporter/cloze.js

--- MoodleExporter/cloze.js
+++ MoodleExporter/cloze.js
@@ -10,7 +10,20 @@ function fr2cloze (qi) {
 }
 
 function mc2cloze (qi) {
-    return "NOT IMPLEMENTED YET";    
+    var text = "<div>" + qi.questionText  +"</div>";
+    var points = ("points" in qi) ? qi.points : 1;
+    var clozeText = "{" + points + ":MCV:";
+
+    for (var i=0; i< qi.distractors.length ; i++) {
+	if (i!=0)
+	    clozeText += "~";
+	if (i==qi.answerIndex) 
+	    clozeText += "=";
+	clozeText += qi.distractors[i];	
+    }
+    
+    clozeText += "}";
+    return text + clozeText;
 }
 
 function label2cloze (qi) {
@@ -25,7 +38,7 @@ function qi2cloze(qi) {
     } 
     
     if ("label" in qi) {
-	return fr2cloze(qi);
+	return label2cloze(qi);
     }
     
     if ("outputType" in qi  && qi.outputType=="fr") {
@@ -33,7 +46,7 @@ function qi2cloze(qi) {
     }
     
     if ("outputType" in qi  && qi.outputType=="mc") {
-	return fr2cloze(qi);
+	return mc2cloze(qi);
     }
 
     throw new Error("invalid argument to qi2cloze: " + JSON.stringify(qi));
--- test/MoodleExporter/cloze.js
+++ test/MoodleExporter/cloze.js
@@ -1,6 +1,13 @@
 var expect = require("chai").expect;
 var cloze = require('../../MoodleExporter/cloze');
 
+// TODO: Need escapeCloze function to escape the special chars that are part of cloze syntax.
+//   SHOULD write escapeCloze that puts \ in front of  } # ~ / " or \
+//   SHOULD call escapeCloze any time  questionText, answer, or distractor is passed through
+// https://docs.moodle.org/23/en/Embedded_Answers_(Cloze)_question_type
+// https://moodle.org/mod/forum/discuss.php?d=275299
+
+
 describe('cloze', function () {
 
     var qi_fr1 = {
@@ -14,8 +21,8 @@ describe('cloze', function () {
     var qi_fr2 = {
         "outputType": "fr",
         "problemType": "paq-change-of-base",
-        "questionText": "Convert 28 from decimal to binary.",
-        "answer": "11100",
+        "questionText": "Convert 2 from decimal to binary.",
+        "answer": "10",
         "title": "This title will be ignored",
 	"points": 42
     };
@@ -36,13 +43,59 @@ describe('cloze', function () {
         "answerIndex": 4
     }
 
+    var qi_mc1 = {
+        "outputType": "mc",
+        "problemType": "paq-change-of-base",
+        "questionText": "Convert 1100 0011 from binary to hexadecimal.",
+        "answer": "c3",
+        "title": "Change of Base",
+        "distractors": [
+            "4a",
+            "195",
+            "67",
+            "303",
+            "c3"
+        ],
+        "answerIndex": 4
+    }
+
+
+    var qi_mc2 = {
+            "outputType": "mc",
+            "problemType": "paq-change-of-base",
+            "questionText": "Convert 1000 from base 2 to base 16.",
+            "answer": "8",
+	    "points": 7,
+            "title": "Change of Base",
+            "distractors": [
+                "22",
+                "dc",
+                "8",
+                "b9"
+            ],
+            "answerIndex": 2
+        }
+
+    
     describe('qi2cloze(questionInstance)', function () {
-	
+
 	it('should return valid cloze for a fr question with no points specified', function() {
 	    var expected = "<div>Convert 28 from decimal to binary.</div>{1:SA:=11100}";
 	    expect(cloze.qi2cloze(qi_fr1)).to.equal(expected);
 	});
+	
+	it('should return valid cloze for a fr question that does have points specified', function() {
+	    var expected = "<div>Convert 2 from decimal to binary.</div>{42:SA:=10}";
+	    expect(cloze.qi2cloze(qi_fr2)).to.equal(expected);
+	});
 
+	it('should return valid cloze for a mc question with no points specified', function() {
+	    var expected = "<div>Convert 1000 from base 2 to base 16.</div>{7:MCV:22~dc~=8~b9}";		
+	    expect(cloze.qi2cloze(qi_mc2)).to.equal(expected);
+	});
+	
+
+	
     });
 
     
